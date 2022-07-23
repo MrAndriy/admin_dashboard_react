d2d073db1a919073078add588b36af7db039df06
DadaLV <vynohradova.l.a@gmail.com>
2022-07-23 21:49:36 +0300

    property list, featured and FeaturedProperties.jsx in client was modified, hooks added

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -1,31 +1,54 @@
-import "./featured.css"
+import useFetch from "../../hooks/useFetch";
+import "./featured.css";
+
+const { data, loading, error } = useFetch(""); //here we must write a path!;
 
 const Featured = () => {
   return (
     <div className="featured">
-      <div className="featuredItem">
-        <img src="https://cf.bstatic.com/xdata/images/city/square250/653082.webp?k=c161c185c16c0402f72a69272e3757ffa3b45f5a28accb4c07a2c989625132cf&o=" alt="" className="featuredImg"/>
-        <div className="featuredTitles">
-          <h1>Warsaw</h1>
-          <h2>123 properties</h2>
-        </div>
-      </div>
-      <div className="featuredItem">
-        <img src="https://cf.bstatic.com/xdata/images/city/square250/972536.webp?k=fc90a871db0baae4bd8b649d9624809eaceee5f4ef218f09d158c8fe8d6d6abe&o=" alt="" className="featuredImg"/>
-        <div className="featuredTitles">
-          <h1>Prague</h1>
-          <h2>123 properties</h2>
-        </div>
-      </div>
-      <div className="featuredItem">
-        <img src="https://cf.bstatic.com/xdata/images/city/square250/653109.jpg?k=39834303d6af8adc39cc8218ea8efc5291a013bd3228d53a50cb836833f4777f&o=" alt="" className="featuredImg"/>
-        <div className="featuredTitles">
-          <h1>Zakopane</h1>
-          <h2>123 properties</h2>
-        </div>
-      </div>
+      {loading ? (
+        "Loading, please wait"
+      ) : (
+        <>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/square250/653082.webp?k=c161c185c16c0402f72a69272e3757ffa3b45f5a28accb4c07a2c989625132cf&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Warsaw</h1>
+              <h2>{data[0]}</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/square250/972536.webp?k=fc90a871db0baae4bd8b649d9624809eaceee5f4ef218f09d158c8fe8d6d6abe&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Prague</h1>
+              <h2>{data[1]}</h2>
+            </div>
+          </div>
+          <div className="featuredItem">
+            <img
+              src="https://cf.bstatic.com/xdata/images/city/square250/653109.jpg?k=39834303d6af8adc39cc8218ea8efc5291a013bd3228d53a50cb836833f4777f&o="
+              alt=""
+              className="featuredImg"
+            />
+            <div className="featuredTitles">
+              <h1>Zakopane</h1>
+              <h2>{data[2]}</h2>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Featured
+export default Featured;
+
+//
