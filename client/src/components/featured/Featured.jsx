8e5c87b278d795b4c488bb8f6fe45dce65518cd0
DadaLV <vynohradova.l.a@gmail.com>
2022-08-02 16:04:25 +0300

    Featured Featured

--- client/src/components/featured/Featured.jsx
+++ client/src/components/featured/Featured.jsx
@@ -6,29 +6,33 @@ const Featured = () => {
   const {data, loading, errer } = useFetch("/hotels/countByCity?cities=Lviv,Rivne,Kyiv")
   return (
     <div className="featured">
-      <div className="featuredItem">
+      {loading ? (
+        "Loadind please wait"
+        ) : 
+        <>
+        <div className="featuredItem">
         <img src="https://cf.bstatic.com/xdata/images/city/square250/653082.webp?k=c161c185c16c0402f72a69272e3757ffa3b45f5a28accb4c07a2c989625132cf&o=" alt="" className="featuredImg"/>
         <div className="featuredTitles">
-          <h1>Warsaw</h1>
-          <h2>123 properties</h2>
+          <h1>Lviv</h1>
+          <h2>{data[0]} properties</h2>
         </div>
       </div>
       <div className="featuredItem">
         <img src="https://cf.bstatic.com/xdata/images/city/square250/972536.webp?k=fc90a871db0baae4bd8b649d9624809eaceee5f4ef218f09d158c8fe8d6d6abe&o=" alt="" className="featuredImg"/>
         <div className="featuredTitles">
-          <h1>Prague</h1>
-          <h2>123 properties</h2>
+          <h1>Rivne</h1>
+          <h2>{data[1]} properties</h2>
         </div>
       </div>
       <div className="featuredItem">
         <img src="https://cf.bstatic.com/xdata/images/city/square250/653109.jpg?k=39834303d6af8adc39cc8218ea8efc5291a013bd3228d53a50cb836833f4777f&o=" alt="" className="featuredImg"/>
         <div className="featuredTitles">
-          <h1>Zakopane</h1>
-          <h2>123 properties</h2>
+          <h1>Kyiv</h1>
+          <h2>{data[2]} properties</h2>
         </div>
-      </div>
+      </div></>}
     </div>
-  )
-}
+  );
+};
 
 export default Featured
