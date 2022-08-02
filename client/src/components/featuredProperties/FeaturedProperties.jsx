460ae0fa6c8840cb219ec83ae86a6c3b1b60387c
MrAndriy <59733657+MrAndriy@users.noreply.github.com>
2022-08-02 22:00:38 +0300

    Liuba 28/07/fetch (#11)
    * Add hooks
    
    * Add refetch
    
    * Add axios
    
    * Featured Featured
    
    * featured propertyList
    
    * Bug in Featured solve
    
    * Featured logo in navbar
    
    * Featured FeaturedProperties
    
    * feat: update component
    
    Co-authored-by: DadaLV <vynohradova.l.a@gmail.com>

--- client/src/components/featuredProperties/FeaturedProperties.jsx
+++ client/src/components/featuredProperties/FeaturedProperties.jsx
@@ -1,52 +1,40 @@
-import "./featuredProperties.css"
+import useFetch from "../../hooks/useFetch";
+import "./featuredProperties.css";
 
 const FeaturedProperties = () => {
+  const { data, loading, error } = useFetch("/hotels?featured=true&limit=4");
   return (
     <div className="fp">
-      <div className="fpItem">
-       <img src="https://cf.bstatic.com/xdata/images/xphoto/540x405/156200800.webp?k=9eb7f14183168922cb5236c988205bb130c621c2d24cfc6b23369994c2d4e862&o=" alt="" className="fpImg" />
-       <span className="fpName">Aparthotel Stare Miasto</span>
-       <span className="fpCity">Madrid</span>
-       <span className="fpPrice">Starting from $123</span>
-       <div className="fpRating">
-         <button>8.9</button>
-         <span>Excellent</span>
-       </div>
-      </div>
-      <div className="fpItem">
-       <img src="https://cf.bstatic.com/xdata/images/xphoto/540x405/156200800.webp?k=9eb7f14183168922cb5236c988205bb130c621c2d24cfc6b23369994c2d4e862&o=" alt="" className="fpImg" />
-       <span className="fpName">Aparthotel Stare Miasto</span>
-       <span className="fpCity">Madrid</span>
-       <span className="fpPrice">Starting from $123</span>
-       <div className="fpRating">
-         <button>8.9</button>
-         <span>Excellent</span>
-       </div>
-      </div>
-      <div className="fpItem">
-       <img src="https://cf.bstatic.com/xdata/images/xphoto/540x405/156200800.webp?k=9eb7f14183168922cb5236c988205bb130c621c2d24cfc6b23369994c2d4e862&o=" alt="" className="fpImg" />
-       <span className="fpName">Aparthotel Stare Miasto</span>
-       <span className="fpCity">Madrid</span>
-       <span className="fpPrice">Starting from $123</span>
-       <div className="fpRating">
-         <button>8.9</button>
-         <span>Excellent</span>
-       </div>
-      </div>
-      <div className="fpItem">
-       <img src="https://cf.bstatic.com/xdata/images/xphoto/540x405/156200800.webp?k=9eb7f14183168922cb5236c988205bb130c621c2d24cfc6b23369994c2d4e862&o=" alt="" className="fpImg" />
-       <span className="fpName">Aparthotel Stare Miasto</span>
-       <span className="fpCity">Madrid</span>
-       <span className="fpPrice">Starting from $123</span>
-       <div className="fpRating">
-         <button>8.9</button>
-         <span>Excellent</span>
-       </div>
-      </div>
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data.map((item) => (
+            <div className="fpItem" key={item._id}>
+              <img
+                src={item.photos[0]}
+                alt=""
+                className="fpImg"
+              />
+              <span className="fpName">{item.name}</span>
+              <span className="fpCity">{item.city}</span>
+              <span className="fpPrice">
+                Starting from ${item.cheapestPrice}
+              </span>
+              {item.rating && (
+                <div className="fpRating">
+                  <button>{item.rating}</button>
+                  <span>Excellent</span>
+                </div>
+              )}
+            </div>
+          ))}
+        </>
+      )}
     </div>
-    
-    
-  )
-}
+  );
+};
 
-export default FeaturedProperties
+export default FeaturedProperties;
+
+//line 5 is the link proper? line 18 from where do we get item?
