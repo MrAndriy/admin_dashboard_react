8717067acd2318b423c28904cabd7dd68c752c99
DadaLV <vynohradova.l.a@gmail.com>
2022-08-02 17:05:37 +0300

    featured propertyList

--- client/src/components/propertyList/PropertyList.jsx
+++ client/src/components/propertyList/PropertyList.jsx
@@ -1,45 +1,37 @@
-import "./propertyList.css"
+import useFetch from "../../hooks/useFetch";
+import "./propertyList.css";
 
 const PropertyList = () => {
+  const { data, loading, error } = useFetch("/hotels/countByType");
+
+  const images = [
+    "https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=",
+    "https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=",
+    "https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=",
+    "https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=",
+    "https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o="
+  ];
   return (
     <div className="pList">
-      <div className="pListItem">
-        <img src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=" alt="" className="pListImg" />
-        <div className="pListTitles">
-          <h1>Hotels</h1>
-          <h2>122 hotels</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=" alt="" className="pListImg" />
-        <div className="pListTitles">
-          <h1>Hotels</h1>
-          <h2>122 hotels</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=" alt="" className="pListImg" />
-        <div className="pListTitles">
-          <h1>Hotels</h1>
-          <h2>122 hotels</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=" alt="" className="pListImg" />
-        <div className="pListTitles">
-          <h1>Hotels</h1>
-          <h2>122 hotels</h2>
-        </div>
-      </div>
-      <div className="pListItem">
-        <img src="https://r-xx.bstatic.com/xdata/images/xphoto/300x240/57584488.jpeg?k=d8d4706fc72ee789d870eb6b05c0e546fd4ad85d72a3af3e30fb80ca72f0ba57&amp;o=" alt="" className="pListImg" />
-        <div className="pListTitles">
-          <h1>Hotels</h1>
-          <h2>122 hotels</h2>
-        </div>
-      </div>
+      {loading ? (
+        "loading"
+      ) : (
+        <>
+          {data &&
+            images.map((img, i) => (
+              <div className="pListItem" key = {i}>
+                <img src={img} alt="" className="pListImg" />
+                <div className="pListTitles">
+                  <h1>hotels</h1> 
+                  <h2>123 hotels</h2>
+                </div>
+              </div>
+            ))}
+        </>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default PropertyList
+export default PropertyList;
+//line 25 {data[i].type} and line 26 {data[i].count}
